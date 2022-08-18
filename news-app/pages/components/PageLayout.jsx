bb31e8947e73cefb7acddff31488fa8ee4b47ba3
teovelilla <teovelilla@gmail.com>
2022-08-17 22:55:07 -0500

    ✨ get news from newsApi and create component to list articles

--- news-app/pages/components/PageLayout.jsx
+++ news-app/pages/components/PageLayout.jsx
@@ -3,9 +3,9 @@ export default function PageLayout({ children, title = "NewsApp" }) {
     return (
         <>
             <Head>
-                <title>{title}</title>
+                <title>{ title }</title>
                 <meta name='description' content='newsapp - the best app to read news'/>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="./favicon.ico" />
             </Head>
             <header>
                 📰 newsapi
--- news-app/pages/index.js
+++ news-app/pages/index.js
@@ -1,13 +1,30 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import { useState, useEffect } from 'react';
 import styles from '../styles/Home.module.css'
 import PageLayout from './components/PageLayout';
 export default function Home() {
+  const [ articles, setArticles ] = useState([])
+  useEffect(()=>{
+    fetch('')
+      .then(response=>response.json())
+      .then(response => {
+        const { articles } = response
+        setArticles(articles)
+      })
+  },[])
   return (
     <PageLayout title="NewsApp - Home">
       <div className={styles.container}>
-        <h1> Aprendiendo Next.js desde cero</h1>
-        <Link href="/about">About</Link>
+        {articles.length === 0 && <p>🏍️Loading...</p>}
+        {articles.length > 0 && articles.map((article, index) => (
+          <article key={index}>
+            <img 
+              alt={`Image for the article ${article.title}`}
+              src={article.urlToImage}
+            />
+            <h2>{article.title}</h2>
+            <p>{article.description}</p>
+          </article>
+        ))}
       </div>
     </PageLayout>
   )
