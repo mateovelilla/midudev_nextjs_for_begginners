ffc05d80ff22dacdd897dac38fd75618f148b681
teovelilla <teovelilla@gmail.com>
2022-08-24 08:54:11 -0500

    ✨  Create comic component with images

--- xkcd-app/next.config.js
+++ xkcd-app/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains:['imgs.xkcd.com']
+  },
   reactStrictMode: true,
   swcMinify: true,
 }
---
+++ xkcd-app/pages/comic/[id].js
@@ -0,0 +1,84 @@
+import Head from 'next/head'
+import { Header } from 'components/Header';
+import Image from 'next/image';
+import { readFile, readdir, stat } from 'fs/promises'
+import { basename } from 'path';
+import Link from 'next/link';
+export default function Comic({
+  img,
+  alt,
+  title,
+  width,
+  height,
+  hasPrevious,
+  hasNext,
+  prevId,
+  nextId
+}) {
+  return <>
+    <Head>
+      <title>xkcd - Comics for developers</title>
+      <meta name="description" content="Comics for developers"/>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Header></Header>  
+    <main>
+      <section className='max-w-lg m-auto'>
+        <h1 className='font-bold text-xl text-center mb-4'>{title}</h1>
+        <div className='max-w-xs m-auto mb-4'>
+          <Image layout='responsive' width={width} height={height} alt={alt} src={img}></Image>
+        </div>
+        <p>{alt}</p>
+        <div className='flex justify-between mt-4 font-bold'>
+          {
+            hasPrevious && <Link href={`/comic/${prevId}`}>
+              <a className='text-gray-600'> 🔙 Prev</a>
+            </Link>
+          }
+          {
+            hasNext && <Link href={`/comic/${nextId}`}>
+              <a className='text-gray-600'> Next 🔜</a>
+            </Link>
+          }
+        </div>
+      </section>
+    </main>
+  </>
+}
+
+export async function getStaticPaths () {
+  const files = await readdir('./comics');
+  return {
+    paths: files.map(file => {
+      return {
+        params: {
+          id: basename(file,'.json')
+        }
+      }
+    }),
+    fallback: false
+  }
+}
+
+export async function getStaticProps({params:{id}}) {
+  const comicFile = await readFile(`../comics/${id}.json`, 'utf8')
+  const comic = JSON.parse(comicFile)
+  const idNumber = +id;
+  const prevId = idNumber - 1;
+  const nextId = idNumber + 1;
+  const [prevResult, nextResult] = await Promise.allSettled([
+    stat(`./comics/${prevId}.json`),
+    stat(`./comics/${nextId}.json`)
+  ])
+  const hasPrevious = prevResult.status === 'fulfilled';
+  const hasNext = nextResult.status === 'fulfilled';
+  return {
+    props: {
+      ...comic,
+      hasPrevious,
+      hasNext,
+      prevId,
+      nextId
+    }
+  }
+}
